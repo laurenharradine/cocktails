a6b1c87601e3143a58ad74b37caaaa7a678afd93
Lauren Harradine <lhharradine@gmail.com>
2023-01-08 18:08:42 +1100

    Pulled the start of a carousel from the depths of Hell itself

--- js/main.js
+++ js/main.js
@@ -1,37 +1,23 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
-// Listen out for a button click
-const btn = document.querySelector("button");
-btn.addEventListener("click", getCocktail);
+document.addEventListener("DOMContentLoaded", getCocktails);
 
-function getCocktail() {
-  const cocktail = document.querySelector("input").value.toLowerCase();
-  // make sure the input field isn't blank
-  if (cocktail !== "") {
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`;
+function getCocktails() {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
 
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        // make sure that a valid cocktail name has been entered; 'drinks' will be null if invalid
-        if (data.drinks != null) {
-          // Go through array of drinks that have the input in its name to find the one we want.
-          // e.g. 'Margarita' returns several variations of margaritas.
-          for (i = 0; i < data.drinks.length; i++) {
-            console.log(i);
-            // If the name matches...
-            if (data.drinks[i].strDrink.toLowerCase() === cocktail) {
-              let drk = data.drinks[i];
-              document.querySelector("h2").innerText = drk.strDrink;
-              document.querySelector("img").src = drk.strDrinkThumb;
-              document.querySelector("h3").innerText = drk.strInstructions;
-            }
-          }
-        }
-      })
-      .catch((err) => {
-        console.log(`error ${err}`);
-      });
-  }
+    for(let i = 1; i <= 5; i++){
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          let drk = data.drinks[0];
+          srcStr = `.gallery-item-${i}`
+          //document.querySelector("h2").innerText = drk.strDrink;
+          document.querySelector(srcStr).src = drk.strDrinkThumb;
+          //document.querySelector("h3").innerText = drk.strInstructions;
+        })
+        .catch((err) => {
+          console.log(`error ${err}`);
+        });
+    }
+  
 }
